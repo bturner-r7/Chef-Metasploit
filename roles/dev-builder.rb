79f082518978e165c637ee529259142f4dd04172
Trevor Rosen <Trevor_Rosen@rapid7.com>
2012-05-27 15:08:12 -0500

    Fix minor mistake

--- roles/dev-builder.rb
+++ roles/dev-builder.rb
@@ -2,5 +2,5 @@ name "dev-builder"
 description "A development environment suitable for executing certain build steps"
 run_list(
   "recipe[postgresql]",
-  "recipe[postgresql][server]"
+  "recipe[postgresql::server]"
 )
