d8ee1f8ebd51d37d0bf8c5b50e04ddc5931de462
Trevor Rosen <Trevor_Rosen@rapid7.com>
2012-05-28 00:00:08 -0500

    More tweakage of package list

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,7 +7,7 @@
 
 # Get deps via apt
 sudo apt-get update
-sudo apt-get install -y build-essential curl libssl-dev libxml2-dev libyaml libxslt-dev libreadline-dev libpq-dev git-core zlib
+sudo apt-get install -y build-essential curl libssl-dev libxml2-dev libyaml-dev libxslt-dev libreadline-dev libpq-dev git-core libzlib-ruby
 
 
 # Install Ruby
