827e22e4ae210ae855c654bfc6ead7a8cc71f9f7
Trevor Rosen <Trevor_Rosen@rapid7.com>
2012-05-27 23:56:15 -0500

    Fix-up bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,24 +4,25 @@
 #
 # Bootstrapping script for boxes managed by Chef solo runs
 
-# Get basics via apt
 
-echo "#-------- Getting dependency packages...\n\n"
+# Get deps via apt
 sudo apt-get update
-sudo apt-get install -y build-essential curl libssl-dev libxml2-dev libxslt-dev libreadline5 libpq-dev git-core
+sudo apt-get install -y build-essential curl libssl-dev libxml2-dev libyaml libxslt-dev libreadline-dev libpq-dev git-core zlib
 
 
-echo "#-------- Installing Ruby...\n\n"
-sudo mkdir /usr/local/src && cd /usr/local/src
+# Install Ruby
+sudo mkdir -p /usr/local/src && cd /usr/local/src
 wget http://ftp.ruby-lang.org/pub/ruby/1.9/ruby-1.9.3-p194.tar.gz
 tar -zxf ruby-1.9.3-p194.tar.gz
 cd ruby-1.9.3-p194
-./configure && make && sudo make install
+./configure --prefix=/usr && make && sudo make install
 
-
-echo "#-------- Installing RubyGems...\n\n"
+# Install RubyGems
 wget http://rubyforge.org/frs/download.php/76073/rubygems-1.8.24.tgz
 cd rubygems-1.8.24 && sudo ruby setup.rb
+
+# Install Chef gem
+sudo gem install chef --no-rdoc --no-ri
 
 echo "Installed `ruby --version`"
 echo "Installed `gem --version`"
