b31dc5a6b379e4309d9d29f9a3f840fef612fa61
Samuel Huckins <samuel_huckins@rapid7.com>
2012-06-25 18:21:52 -0500

    Added Testlink source file. Now copies and extracts installation files, sets up DB and tables. [Story #31550157]

--- cookbooks/testlink/recipes/default.rb
+++ cookbooks/testlink/recipes/default.rb
@@ -5,18 +5,46 @@
 # -----------------------------
 
 # Create application dir
-
+directory "/var/www/testlink" do
+  mode "0644"
+  group "root"
+  owner "root"
+  action :create
+end
 
 # Copy installer file, expand
+cookbook_file "/tmp/testlink-1.9.2.tar.gz" do
+  source "testlink-1.9.2.tar.gz"
+  mode 0755
+  owner "root"
+  group "root"
+end
 
-# Configure Apache?
+execute "file-extraction_perm-fix" do
+  user "root"
+  command "tar -xzf /tmp/testlink-1.9.2.tar.gz -C /var/www/testlink"
+  command "mv /var/www/testlink/testlink-1.9.2/* /var/www/testlink"
+  command "rm -rf /var/www/testlink/testlink-1.9.2"
+  command "chown -R root:root /var/www/testlink"
+  # TODO Fix chmod: templates_c, upload_area and logs apache writeable
+end
 
 # Add DB and tables, user
+execute "DB-setup" do
+  user "root"
+  command "mysql -u root -proot -e 'CREATE DATABASE testlink'"
+  command "mysql -u root -proot testlink < /var/www/testlink/install/sql/mysql/testlink_create_tables.sql"
+  command "mysql -u root -proot testlink < /var/www/testlink/install/sql/mysql/testlink_create_default_data.sql"
+  # TODO Setup DB user for testlink
+end
 
 # PHP config file
+# config_db.inc.php
 
-# chmod files, app config
+# Configure Apache to serve
+
+# Remove install dir
+
 
 # iptables
-
 # Add other allowed users
