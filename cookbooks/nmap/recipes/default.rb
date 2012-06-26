0e6c8c115953743d5645ce32b13c304ebdb20918
Samuel Huckins <samuel_huckins@rapid7.com>
2012-06-26 15:58:25 -0500

    Updated to install nmap from source, completes successfully. [Story #31851859] Old version of this recipe was attempting to install from Debian repo. This has an even older version than the base Ubuntu repos.

--- cookbooks/nmap/recipes/default.rb
+++ cookbooks/nmap/recipes/default.rb
@@ -1,8 +1,36 @@
-include_recipe "apt"
+# Debian stable package: 5.0
+# Ubuntu repos stable: 5.21
+# Actual latest: 6.01
+#
+# MS currently ships with 5.61TEST4
 
-apt_repository "nmap" do
-  uri "http://packages.debian.org/stable/nmap"
-  notifies :run, "execute[apt-get update]", :immediately
+# TODO Make this OS independent:
+package "build-essential"
+
+installer_file = "nmap-6.01.tar.bz2"
+expanded_dir = "nmap-6.01"
+
+cookbook_file "/tmp/#{installer_file}" do
+  source installer_file
+  mode 0755
+  owner "root"
+  group "root"
 end
 
-package "nmap"
+execute "extract-cd" do
+  user "root"
+  command "cd /tmp && bzip2 -cd #{installer_file} | tar xf - "
+end
+
+execute "configure-make-install" do
+  user "root"
+  command "cd /tmp/#{expanded_dir} &&
+           ./configure &&
+           make &&
+           make install"
+end
+
+execute "version" do
+  user "root"
+  command "nmap --version"
+end
