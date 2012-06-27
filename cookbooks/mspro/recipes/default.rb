293b712fb9faafe3e907e82f653792917164c1c0
Samuel Huckins <samuel_huckins@rapid7.com>
2012-06-27 13:30:23 -0500

    Changed logging level when failing.

--- cookbooks/mspro/recipes/default.rb
+++ cookbooks/mspro/recipes/default.rb
@@ -3,5 +3,5 @@ msbuilder_created = File.exists? "/home/#{node['user']}"
 if msbuilder_created
   include_recipe "mspro::conditional-mspro"
 else
-  Chef::Log.info "/home/#{node['user']} does not exist. Create this user and add a GitHub-enabled private key to continue with dev setup."
+  Chef::Log.fatal "/home/#{node['user']} does not exist. Create this user and add a GitHub-enabled private key to continue with dev setup."
 end
