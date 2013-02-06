05b8742f700b9fbad8547597df2cf03dba138443
Trevor Rosen <Trevor_Rosen@rapid7.com>
2013-02-05 21:43:48 -0600

    Go manual if you want websploit tests
    * Websploit tests commented out by default, as they require access to private repos

--- cookbooks/web-target/recipes/default.rb
+++ cookbooks/web-target/recipes/default.rb
@@ -11,6 +11,10 @@ include_recipe "web-target::shellol"
 include_recipe "web-target::sqlol"
 include_recipe "web-target::tikiwiki"
 include_recipe "web-target::webdav"
-include_recipe "web-target::websploit-tests"
+
+# Uncomment to install R7-proprietary tests
+# requires SSH key
+# include_recipe "web-target::websploit-tests"
+
 include_recipe "web-target::xmlmao"
 include_recipe "web-target::xssmh"
