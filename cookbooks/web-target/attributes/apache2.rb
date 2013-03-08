ecfacc4d80a79dc2a342ca512520374e28ac93ab
Brandon Turner <bt@brandonturner.net>
2013-03-08 12:22:39 -0600

    Enable insecure SSL in web-target cookbook

---
+++ cookbooks/web-target/attributes/apache2.rb
@@ -0,0 +1,2 @@
+default['apache']['mod_ssl']['cipher_suite'] = 'LOW:HIGH:MEDIUM:+EXP:+SHA1:+MD5:+LOW:+HIGH:+MEDIUM'
+default['apache']['mod_ssl']['protocol'] = '+all'
--- cookbooks/web-target/recipes/apache2.rb
+++ cookbooks/web-target/recipes/apache2.rb
@@ -4,6 +4,16 @@ package 'php5-mysql'
 include_recipe "apache2"
 include_recipe "apache2::mod_dav_fs"
 include_recipe "apache2::mod_php5"
+include_recipe "apache2::mod_ssl"
+
+# HACK to override the template in apache2::mod_ssl
+begin
+  t = resources(:template => "#{node['apache']['dir']}/mods-available/ssl.conf")
+  t.source "apache2-ssl.conf.erb"
+  t.cookbook "web-target"
+rescue Chef::Exceptions::ResourceNotFound
+  Chef::Log.warn "could not find template mods/ssl.conf.erb to modify"
+end
 
 apache_site "default"
 apache_site "default-ssl"
