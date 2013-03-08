5f7fd8cfe64260b8cd61c09aa0f80b17f723e09b
Brandon Turner <bt@brandonturner.net>
2013-03-08 12:03:16 -0600

    Update Apache cookbook to latest version (1.6.0)

--- cookbooks/web-target/recipes/apache2.rb
+++ cookbooks/web-target/recipes/apache2.rb
@@ -5,6 +5,9 @@ include_recipe "apache2"
 include_recipe "apache2::mod_dav_fs"
 include_recipe "apache2::mod_php5"
 
+apache_site "default"
+apache_site "default-ssl"
+
 file "/var/www/index.html" do
   action :delete
 end
