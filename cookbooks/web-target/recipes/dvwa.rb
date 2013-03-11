562ccf5d6f0a1f5ddc50d472cd24e2c9332dbf94
Brandon Turner <bt@brandonturner.net>
2013-03-11 16:42:13 -0500

    Reload apache before configuring dvwa

--- cookbooks/web-target/recipes/dvwa.rb
+++ cookbooks/web-target/recipes/dvwa.rb
@@ -15,6 +15,13 @@ template "/var/www/dvwa/config/config.inc.php" do
   source "dvwa-config.php.erb"
 end
 
+# Reload apache configuration before setting up dvwa.
+# Chef postpones service reloads until the end of the run, but
+# configuring DVWA depends on apache being configured correctly.
+service "apache2" do
+  action :reload
+  not_if { FileTest.directory?("/var/lib/mysql/#{node['web-target']['dvwa']['db_name']}") }
+end
 execute "create-database" do
   command 'wget -q -o /dev/null --post-data="create_db=Create"  http://127.0.0.1/dvwa/setup.php'
   not_if { FileTest.directory?("/var/lib/mysql/#{node['web-target']['dvwa']['db_name']}") }
