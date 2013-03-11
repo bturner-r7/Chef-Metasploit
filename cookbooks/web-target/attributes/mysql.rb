f60cdde06384ffcdc03f75fc96437b6cb7ce0695
Brandon Turner <bt@brandonturner.net>
2013-03-11 16:41:16 -0500

    Bind MySQL to localhost instead of current IP
    Fixes a bug when the IP changes but chef hasn't run.

--- cookbooks/web-target/attributes/mysql.rb
+++ cookbooks/web-target/attributes/mysql.rb
@@ -2,3 +2,4 @@
 default[:mysql][:server_root_password] = 'msfadmin'
 default[:mysql][:server_debian_password] = 'msfadmin'
 default[:mysql][:server_repl_password] = 'msfadmin'
+default[:mysql][:bind_address] = '127.0.0.1'
